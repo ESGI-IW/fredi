013708d5e5ce760453c492fcc734419dd5a178d6
Luffy <kosseyla.hamouche@gmail.com>
2018-10-18 14:49:27 +0200

    bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,9 +18,17 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new fredi\AppBundle\frediAppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         ];
+
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
 
         return $bundles;
     }
